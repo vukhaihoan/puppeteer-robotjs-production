647b31067279718cd746ba8935df88ce09903054
vukhaihoan <58582870+vukhaihoan@users.noreply.github.com>
2022-03-04 20:36:26 +0700

    Delete 1other directory

--- 1other/out.js
+++
@@ -1,6 +0,0 @@
-var head = document.getElementsByTagName("head").item(0);
-var script = document.createElement("script");
-script.setAttribute("async", "true");
-script.setAttribute("defer", "true");
-script.setAttribute("src", "//client-api.arkoselabs.com/v2/B7D8911C-5CC8-A9A3-35B0-554ACEE604DA/api.js");
-head.appendChild(script);
